3c3bd4d0160c4ec2a4e6fa0b4f20173407bc8a7a
Paulo Eduardo Correa <paulo.correa@sytac.io>
2024-10-01 15:58:26 +0200

    Base Implementation

--- Sources/Validator/Validator.swift
+++ Sources/Validator/Validator.swift
@@ -1,2 +1,22 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  Validator.swift
+//  Validator
+//
+//  Created by Paulo Correa on 29/09/2024.
+//
+
+import Foundation
+
+public struct PlaceLocationValidator {
+    public static func isValidLatitude(_ latitudeInput: String) -> Bool {
+        let latitudeRegex = #"^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)$"#
+        let latitudePredicate = NSPredicate(format: "SELF MATCHES %@", latitudeRegex)
+        return latitudePredicate.evaluate(with: latitudeInput)
+    }
+
+    public static func isValidLongitude(_ longitudeInput: String) -> Bool {
+        let longitudeRegex = #"^[-+]?((180(\.0+)?)|((\d{1,2}|1[0-7]\d)(\.\d+)?))$"#
+        let longitudePredicate = NSPredicate(format: "SELF MATCHES %@", longitudeRegex)
+        return longitudePredicate.evaluate(with: longitudeInput)
+    }
+}
